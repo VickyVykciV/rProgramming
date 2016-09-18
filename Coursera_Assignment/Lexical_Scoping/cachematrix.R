b215a4040f9b90af3c4e5246feda7e2ca1d3e93e
Vignesh Venugopal <vickyv@live.in>
2016-09-18 20:21:58 +0530

    Adding explanatory comments

--- Coursera_Assignment/Lexical_Scoping/cachematrix.R
+++ Coursera_Assignment/Lexical_Scoping/cachematrix.R
@@ -1,5 +1,7 @@
-#We are using Lexical Scoping to cache a data
+#Here we are using Lexical Scoping to cache a data and using it for later purpose, 
+#instead of repeating the certain operation again and again
 
+#This specific function holds return the list of functions which helps you to cache the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -14,6 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
+#This function returns the inverse of a data
+#It inturn calls the above function to stores(cache) the inverse of a matrix, for the first time
+#For further calls, it will return the matrix inverse from the cache instead of calculating it again
 cacheSolve <- function(x, ...) {
     m <- x$getinverse()
     if(!is.null(m)) {
