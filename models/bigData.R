b2712d6ce2f42c0eb9195285f202df3c26cf7066
Vignesh Venugopal <vickyv@live.in>
2017-04-05 18:51:46 +0530

    ML in Spark

--- models/bigData.R
+++ models/bigData.R
@@ -1,5 +1,5 @@
 #Installing and Adding libraries
-setwd(dir = "C:/Users/vignesh.venugopal/Documents/GitHub/rProgramming/dataFiles/")
+setwd(dir = "C:/Users/vignesh.venugopal/Documents/GitHub/rProgramming/")
 #source('https://bioconductor.org/biocLite.R')
 #biocLite('rhdf5')
 library(rhdf5)
@@ -7,10 +7,12 @@ library(sparklyr)
 library(dplyr)
 library(moments)
 library(corrplot)
+library(Boruta)
 #source("http://goo.gl/UUyEzD")
+options(scipen=999)
 
 ## Reading the h5 file
-completeData <- h5read(file="C:/Users/vignesh.venugopal/Documents/GitHub/rProgramming/dataFiles/train.h5", name="train")
+completeData <- h5read(file="dataFiles/train.h5", name="train")
 columns <- completeData$axis0
 completeData <- data.frame(as.double(completeData$block0_values[1,]), as.double(completeData$block0_values[2,]), t(completeData$block1_values))
 colnames(completeData) <- columns
@@ -21,27 +23,32 @@ completeY <- data.frame(completeData[, ncol(completeData)])
 colnames(completeY) <- 'y'
 completeData <- completeData[-ncol(completeData)]
 
+## Removing technical_16 variable, as it is having all Zeros
+
+completeData <- completeData[, -which(names(completeData) %in% c('technical_16'))]
+
 ## Numeric Summmary
 
-numericSummaryDF <- data.frame(matrix(0, ncol = 20, nrow = ncol(completeData)))
-colnames(numericSummaryDF) <- c('Feature', 'Length', 'MissingValues', 'ZeroValues', 'UniquePCT', 'Mean', 'STD', 'X0',
+numericSummaryDF <- data.frame(matrix(0, ncol = 19, nrow = ncol(completeData)))
+colnames(numericSummaryDF) <- c('Length', 'MissingValues', 'ZeroValues', 'UniquePCT', 'Mean', 'STD', 'X0',
                                 'X10', 'X20', 'X30', 'X40', 'X50', 'X60', 'X70', 'X80', 'X90', 'X95', 'X99', 'X100')
+rownames(numericSummaryDF) <- colnames(completeData)
 
-NumericSummary <- function(){
-  for (i in 1:ncol(completeData)){
-    x <- completeData[, i]
-    numericSummaryDF[i, ] <- data.frame(colnames(completeData)[i],
-                               length(x),
-                               sum(is.na(x)),
-                               sum(x == 0,na.rm = T),
-                               length(unique(x))/length(x),
-                               mean(x, na.rm = T),
-                               sd(x, na.rm = T),
-                               1,0,0,0,0,0,0,0,0,0,0,0,0)
-  }
+fn_numericSummary <- function(var,datafile){
+  x <- datafile[,var]
+  return(data.frame(length = length(x),
+                    MissingValues = sum(is.na(x)),
+                    ZeroValues = sum(x==0,na.rm=T),
+                    UniquePCT = length(unique(x))/length(x),
+                    Mean = mean(x,na.rm=T),
+                    STD = sd(x,na.rm=T),
+                    data.frame(t(quantile(x,p=sort(c(seq(0,1,.1),.95,.99)), na.rm = T)))
+  ))
 }
 
-NumericSummary()
+for(i in 1:ncol(completeData)){
+  numericSummaryDF[i,] <- fn_numericSummary(colnames(completeData)[i], completeData)[1, ]
+}
 
 ## Calculating NA/NaNs percentage in each variable
 
@@ -83,11 +90,11 @@ rm(i)
 ## Checking for change in Skewness and Kurtosis of data after imputation
 
 numberOfVaraibles <- length(validVariableList)
-compareDist <- data.frame(matrix(0, ncol = 5, nrow = numberOfVaraibles))
-colnames(compareDist) <- c('VariableName', 'SkwenessBeforeImputation', 'SkwenessAfterImputation', 'KurtosisBeforeImputation', 'KurtosisAfterImputation')
+compareDist <- data.frame(matrix(0, ncol = 4, nrow = numberOfVaraibles))
+colnames(compareDist) <- c('SkwenessBeforeImputation', 'SkwenessAfterImputation', 'KurtosisBeforeImputation', 'KurtosisAfterImputation')
+rownames(compareDist) <- colnames(trimmedData)
 
 for(i in 1:numberOfVaraibles){
-  compareDist$VariableName[i] <- colnames(imputedData)[i]
   compareDist$SkwenessBeforeImputation[i] <- skewness(trimmedData[, i], na.rm = T)
   compareDist$SkwenessAfterImputation[i] <- skewness(imputedData[, i])
   compareDist$KurtosisBeforeImputation[i] <- kurtosis(trimmedData[, i], na.rm = T)
@@ -96,15 +103,23 @@ for(i in 1:numberOfVaraibles){
 
 rm(i, trimmedData , numberOfVaraibles, validVariableList)
 
+## Correlation Matrix, between all Dependent Variable and each Independent Variable
+
+correlationDF <- data.frame(matrix(0, ncol = 1, nrow = ncol(imputedData)))
+colnames(correlationDF) <- 'Corrleation Value'
+rownames(correlationDF) <- colnames(imputedData)
+
+for (i in 1:ncol(imputedData)){
+  correlationDF[i,1] <- as.numeric(cor(imputedData[, i], completeY))
+}
+
+rm(i)
+
 ## Merging Dependent and Independent variables before splitting
 
 imputedData <- cbind.data.frame(imputedData, completeY)
 
 rm(completeY)
-
-## Correlation Matrix
-
-corrplot(cor(imputedData), method = "circle")
 
 ## Splitting data into train and test
 
@@ -116,4 +131,29 @@ testData <- imputedData[-train_ind, ]
 
 rm(imputedData, smp_size, train_ind)
 
+## Starting a Spark Session
 
+config <- spark_config()
+config$spark.local.dir <- 'C:/Users/vignesh.venugopal/Documents/GitHub/rProgramming/models'
+sc <- spark_connect(master = "local", config = config)
+
+## Copying the Train and Test data to Spark
+
+testDF <- copy_to(sc, testData)
+trainDF <- copy_to(sc, trainData)
+
+## Linear - Model
+
+independentVar <- colnames(trainData[, -ncol(trainData)])
+fit <- trainDF%>%ml_linear_regression(response = 'y', features = independentVar)
+summary(fit)
+
+# fit1 <- lm(y~., data = trainData)
+# summary(fit1)
+# boruta.train <- Boruta(y~., data = trainData, doTrace = 2)
+
+rfModel <- ml_random_forest(trainDF, response = 'y', features = independentVar)
+summary(rfModel)
+
+xgboostModel <- ml_gradient_boosted_trees(trainDF, response = 'y', features = independentVar, type = 'regression')
+summary(xgboostModel)
